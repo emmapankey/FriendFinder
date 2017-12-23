475819594220af4ebcbb0b6d13eba14bb706b5b2
emmapankey <emmapankey19@gmail.com>
2017-12-23 10:45:20 -0500

    added data to friends.js

--- data/friends.js
+++ data/friends.js
@@ -3,19 +3,147 @@
 
 var friendsData = [
     {
-        "name": "Ahmed",
-        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "name": "Sunshine Chaney",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/17553789_1472511332759761_3756550400487713104_n.jpg?oh=68e34b6336e4e50f665900d3b1d7667b&oe=5ABD444B",
         "scores": [
+            1,
+            1,
             5,
+            4,
             1,
             4,
+            3,
             4,
+            5,
+            3
+        ]
+    },
+    {
+        "name": "Rob Hewett",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10702201_1472391659707987_2495886528119985855_n.jpg?oh=531d54d4fb3ef2ebd03a87d3448f44fb&oe=5ACF43FF",
+        "scores": [
+            5,
+            4,
+            1,
+            5,
+            5,
+            3,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Jodi Swain",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10525631_577438417613_7724025251962912307_n.jpg?oh=4c1a642ac4543030cc18d669a74ffc8f&oe=5AC6330C",
+        "scores": [
+            2,
+            5,
+            5,
+            4,
+            4,
+            4,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Amy Swain",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/601688_662423212074_1700030549_n.jpg?oh=eb278da7e5a4aacd24e13746235f2573&oe=5AC8F18F",
+        "scores": [
+            4,
+            3,
             5,
             1,
             2,
             5,
             4,
+            5,
+            5,
+            4
+        ]
+    },
+    {
+        "name": "Melissa Swain",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/14729249_10209464205975772_6478516901760757099_n.jpg?oh=5722fab2b9ab12eb5a683a0662235627&oe=5ABA1035",
+        "scores": [
+            2,
+            4,
+            2,
+            2,
+            3,
+            2,
+            3,
+            5,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Dustin Eager",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10309523_10152748856939714_3754589550845867347_n.jpg?oh=3f442dc0e4006d26b888b17b50525e4d&oe=5AB6859F",
+        "scores": [
+            3,
+            4,
+            1,
+            4,
+            5,
+            3,
+            1,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Nikki Forziati",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/15242025_10207823492127327_6731374026518328330_n.jpg?oh=6b9c0fce0f5dfa0ebbc3f8b2e3324c46&oe=5ABC52AA",
+        "scores": [
+            3,
+            2,
+            3,
+            3,
+            1,
+            2,
+            4,
+            3,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "Dennis Hackett",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/10410603_10205178024353357_9095283307613268659_n.jpg?oh=933153e9dddf9275b0b5b921e7b400d5&oe=5ABCA10B",
+        "scores": [
+            5,
+            4,
+            2,
+            5,
+            4,
+            2,
+            4,
+            4,
+            2,
             1
+        ]
+    },
+    {
+        "name": "Fair Isle Loki",
+        "photo": "https://scontent-ort2-2.xx.fbcdn.net/v/t1.0-9/1013640_10152554293466593_2363819484755694247_n.jpg?oh=e28901e4e8d9758e3b780efd82f72217&oe=5AFD91AB",
+        "scores": [
+            4,
+            3,
+            1,
+            5,
+            2,
+            4,
+            1,
+            1,
+            1,
+            2
         ]
     }
 ];

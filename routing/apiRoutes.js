ff3fb13d8e857daa1386b214e5686ed6d0e62afe
emmapankey <emmapankey19@gmail.com>
2017-12-23 22:17:59 -0500

    update post route

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,6 +1,6 @@
 // LOAD DATA
 // Link routes to the friend data array
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 // console.log(friendsData);
 
 // Routes
@@ -13,15 +13,36 @@ module.exports = function (app) {
 
 // POST route takes in user info and compares to the friend data array
   app.post("/api/friends", function (req, res) {
-    var newFriend = {
-      name: req.body.name,
-      photo: req.body.photo,
-      scores: req.body.scores
-    };
+    // User's scores
+    var userScores = req.body.scores;
 
-    for(var i = 0; i < newFriend.scores.length; i++) {
-      
+    // Will hold the scores result after comparison of the user's scores to the existing friends array
+    var matchedScoresArray = [];
+
+    var match = 0;
+
+    // Iterate through the friend objects contained within friends data array
+    for (var i = 0; i < friendsData.length; i++) {
+      var questionDifference = 0;
+      var comparedFriendScores = friendsData[i].scores;
+
+      // Iterate through the user scores and compare to each friend object scores
+        for (var j = 0; j < comparedFriendScores.length; j++)
+          questionDifference = Math.abs(parseInt(userScores[i].scores) - parseInt(comparedFriendScores[j]));
+
+    }
+
+    matchedScoresArray.push(questionDifference);
+
+    for (var i = 0; i < matchedScoresArray.length; i++) {
+      if(matchedScoresArray[i] <= matchedScoresArray[match]) {
+        match = i;
+      }
     }
     
+    friendsData.push(req.body);
+
+    res.json(friendsData[match]);
+
   });
 };

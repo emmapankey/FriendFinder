8c54b45ae497faea0689655aa2333842ea341a12
emmapankey <emmapankey19@gmail.com>
2017-12-28 13:49:13 -0500

    infinite loop

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -1,27 +1,54 @@
 // LOAD DATA
 // Link routes to the friend data array
-var friendsData = require("../data/friends");
+var friendsData = require("../data/friends.js");
 // console.log(friendsData);
 
 // Routes
-
-// GET route returns friend data array in JSON format
 module.exports = function (app) {
+
+  // GET route returns friend data array in JSON format
   app.get("/api/friends", function (req, res) {
     res.json(friendsData);
   });
 
-// POST route takes in user info and compares to the friend data array
+  // POST route takes in user info and compares to the friend data array
   app.post("/api/friends", function (req, res) {
-    var newFriend = {
-      name: req.body.name,
-      photo: req.body.photo,
-      scores: req.body.scores
-    };
 
-    for(var i = 0; i < newFriend.scores.length; i++) {
-      
+    var userData = req.body;
+    // console.log(userData);
+
+    var postedScores = userData.scores;
+    // console.log(postedScores);
+
+    // Will hold the scores result after comparison of the user's scores to the existing friends array
+    var matchedScoresArray = [];
+
+    var match = 0;
+
+    var questionDifference = 0;
+
+    // Iterate through the friend objects contained within friends data array
+    for (var i = 0; i < friendsData.length; i++) {
+      var comparedFriendScores = friendsData[i].scores;
+
+      // Iterate through the user scores and compare to each friend object scores
+      for (var j = 0; j < postedScores.length; j++) {
+        // console.log(questionDifference);
+        questionDifference = Math.abs(parseInt(comparedFriendScores[j]) - parseInt(postedScores[j]));
+      }
+
+      matchedScoresArray.push(questionDifference);
+      console.log(matchedScoresArray);
+
+      // for (var i = 0; i < matchedScoresArray.length; i++) {
+      //   if (matchedScoresArray[i] <= matchedScoresArray[match]) {
+      //     match = i;
+      //   }
+      // }
+
+      friendsData.push(req.body);
+
+      // res.json(friendsData[match]);
     }
-    
   });
 };
